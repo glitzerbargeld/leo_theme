6d6f3cf0671563dd32e130f3fd3ee930e6e16238
glitzerbargeld <torben.jaeckel@me.com>
2021-05-08 15:59:00 +0200

    bottleanimation

--- js/my-rellax.js
+++ js/my-rellax.js
@@ -1,6 +1,6 @@
 
 var rellax = new Rellax('.rellax-bottle', {
-    speed: -4,
+    speed: -2,
     center: true,
     wrapper: null,
     round: true,
@@ -9,7 +9,7 @@ var rellax = new Rellax('.rellax-bottle', {
   });
 
   var rellax = new Rellax('.rellax-cap', {
-    speed: 5,
+    speed: 3,
     center: true,
     wrapper: null,
     round: true,
