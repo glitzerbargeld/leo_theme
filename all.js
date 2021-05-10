130e417dc8a45c08f7b888d6401a8e269fdd5a0e
glitzerbargeld <torben.jaeckel@me.com>
2021-05-10 11:34:06 +0200

    product menu fix

--- all.js
+++ all.js
@@ -108,4 +108,30 @@ $(document).ready(function () {
     }]
   });
 });
+"use strict";
+
+var rellax = new Rellax('.rellax-bottle', {
+  speed: -3.5,
+  center: true,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
+var rellax = new Rellax('.rellax-cap', {
+  speed: 2.5,
+  center: true,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
+var rellax = new Rellax('.rellax-faq', {
+  speed: 3,
+  center: true,
+  wrapper: null,
+  round: true,
+  vertical: false,
+  horizontal: true
+});
 //# sourceMappingURL=all.js.map
