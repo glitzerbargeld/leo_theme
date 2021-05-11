04c110272cf9f175c291a06ccbb05c6f60ae4f4b
glitzerbargeld <torben.jaeckel@me.com>
2021-05-11 14:01:52 +0200

    slider

--- woocommerce/content-single-product-oil.php
+++ woocommerce/content-single-product-oil.php
@@ -121,6 +121,7 @@ slider.addEventListener("change", function(){
 console.log("change");
 let element = document.getElementById("anteil-cbd");
 element.value = this.value + "%";
+element.dispatchEvent(new Event('change'));
 
 console.log(element.value);
 
