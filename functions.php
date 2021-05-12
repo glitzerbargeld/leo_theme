0a05f4c6f292f218df0ed5fbd61c2f3f6ba2cf7e
glitzerbargeld <torben.jaeckel@me.com>
2021-05-12 16:53:53 +0200

    added touch to jquery

--- functions.php
+++ functions.php
@@ -37,6 +37,8 @@ function child_enqueue_scripts(){
 	wp_enqueue_script('rellax-js', get_stylesheet_directory_uri() . '/js/rellax-master/rellax.min.js', array(), false, true);
     wp_enqueue_script('my-rellax', get_stylesheet_directory_uri() . '/js/my-rellax.js', array(), false, true);
     wp_enqueue_script( 'jquery-ui-slider' );
+    wp_enqueue_script('jquery-ui-touchpunch', get_stylesheet_directory_uri() . '/js/jquery.ui.touch-punch.min.js', array(), false, true);
+
     
 }
 
