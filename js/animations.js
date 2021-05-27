8f9df54361a4bde0b0f8819a02c649c10997e71b
glitzerbargeld <torben.jaeckel@me.com>
2021-05-27 13:58:46 +0200

    glass container

--- js/animations.js
+++ js/animations.js
@@ -142,3 +142,8 @@ jQuery( function() {
       
     });
   } );
+
+
+  /*PRODUCT VARIATIONS GLÄSER CBD BLÜTEN*/
+
+  
--- woocommerce/content-single-product-buds.php
+++ woocommerce/content-single-product-buds.php
@@ -139,25 +139,7 @@ if ( post_password_required() ) {
 
 <script>
 
-
-
-	// EventListener hinzufügen
-window.addEventListener("load", function(){
-
-// Range-Slider in Variable speichern 
-var slider = document.querySelector("input[type='range']");
-
-// EventListener für das Verändern des Sliders hinzufügen
-slider.addEventListener("change", function(){
-
-console.log("change");
-let element = document.getElementById("anteil-cbd");
-element.value = this.value + "%";
-element.dispatchEvent(new Event('change'))
-console.log(element.value);
-
-
-});
-});
+glasses = jQuery(".glass");
+window.alert("sometext");
 
 </script> 
