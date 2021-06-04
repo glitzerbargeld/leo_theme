b2855397c6d4511bbb3b5c9a690a9bc91015e68c
glitzerbargeld <torben.jaeckel@me.com>
2021-06-04 16:32:17 +0200

    xx

--- woocommerce/content-single-product-buds.php
+++ woocommerce/content-single-product-buds.php
@@ -29,10 +29,7 @@ defined( 'ABSPATH' ) || exit;
 global $product;
 
 
-add_action('woocommerce_before_single_product', 'entferne_single_excerpt');
-function entferne_single_excerpt(){
-	remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
-}
+
 
 
 
@@ -87,7 +84,6 @@ if ( post_password_required() ) {
 				 * @hooked WC_Structured_Data::generate_product_data() - 60
 				 */
 				add_action( 'woocommerce_before_add_to_cart_button', 'container_size_buds' );
-				
 				add_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt' , 1);
 				do_action( 'woocommerce_single_product_summary' );
 				?>
