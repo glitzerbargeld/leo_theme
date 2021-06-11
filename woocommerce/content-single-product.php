51b9914b0c10406d9da51f2b0e6a0b1cb74c8ccd
glitzerbargeld <torben.jaeckel@me.com>
2021-06-11 15:21:27 +0200

    xx

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -1,10 +1,3 @@
-<style>
-.wp-post-image{
-clip-path: url("#zuschnitt");
-}
-</style>
-
-
 <?php
 /**
  * The template for displaying product content in the single-product.php template
@@ -40,54 +33,33 @@ if ( post_password_required() ) {
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 
-	
+	<?php
+	/**
+	 * Hook: woocommerce_before_single_product_summary.
+	 *
+	 * @hooked woocommerce_show_product_sale_flash - 10
+	 * @hooked woocommerce_show_product_images - 20
+	 */
+	do_action( 'woocommerce_before_single_product_summary' );
+	?>
 
-
-	<div class="ast-row">
-		<div class="ast-col-md-5 ast-col-xs-12 ast-col-md-push-6">
-			<?php
-				/**
-				 * Hook: woocommerce_before_single_product_summary.
-				 *
-				 * @hooked woocommerce_show_product_sale_flash - 10
-				 * @hooked woocommerce_show_product_images - 20
-				 */
-				do_action( 'woocommerce_before_single_product_summary' );
-
-			?>
-		</div>
-
-		<div class="ast-col-md-4 ast-col-xs-12 ast-col-md-pull-4">
-
-			
-
-			<?php
-				/**
-				 * Hook: woocommerce_single_product_summary.
-				 *
-				 * @hooked woocommerce_template_single_title - 5
-				 * @hooked woocommerce_template_single_rating - 10
-				 * @hooked woocommerce_template_single_price - 10
-				 * @hooked woocommerce_template_single_excerpt - 20
-				 * @hooked woocommerce_template_single_add_to_cart - 30
-				 * @hooked woocommerce_template_single_meta - 40
-				 * @hooked woocommerce_template_single_sharing - 50
-				 * @hooked WC_Structured_Data::generate_product_data() - 60
-				 */
-				
-				do_action( 'woocommerce_single_product_summary' );
-				
-
-		</div>
-		
-
-
+	<div class="summary entry-summary">
+		<?php
+		/**
+		 * Hook: woocommerce_single_product_summary.
+		 *
+		 * @hooked woocommerce_template_single_title - 5
+		 * @hooked woocommerce_template_single_rating - 10
+		 * @hooked woocommerce_template_single_price - 10
+		 * @hooked woocommerce_template_single_excerpt - 20
+		 * @hooked woocommerce_template_single_add_to_cart - 30
+		 * @hooked woocommerce_template_single_meta - 40
+		 * @hooked woocommerce_template_single_sharing - 50
+		 * @hooked WC_Structured_Data::generate_product_data() - 60
+		 */
+		do_action( 'woocommerce_single_product_summary' );
+		?>
 	</div>
-
-
-
-
-	
 
 	<?php
 	/**
@@ -102,22 +74,3 @@ if ( post_password_required() ) {
 </div>
 
 <?php do_action( 'woocommerce_after_single_product' ); ?>
-
-
-
-<script>
-jQuery(function($){
-	var price = <?php echo $product->get_price(); ?>,
-		currency = '<?php echo get_woocommerce_currency_symbol(); ?>';
-
-	$('[name=quantity]').change(function(){
-		if (!(this.value < 1)) {
-
-			var product_total = parseFloat(price * this.value);
-
-			$('#product_total_price .price').html( currency + product_total.toFixed(0));
-
-		}
-	});
-});
-</script>
