23a12b193d4f921320e494e40b0dd500bcd9eb16
glitzerbargeld <torben.jaeckel@me.com>
2021-06-17 11:28:14 +0200

    xx

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,4 @@
 /*STORE LOCATOR CUSTOMIZATIONS*/
 
 document.getElementById('wpsl-search-input').placeholder='Ort';
-document.getElementById('wpsl-search-btn').value='Suche';
+document.getElementById('wpsl-search-btn').value='Los';
