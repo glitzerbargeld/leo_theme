994d8cadeb830111303b2d993233f833c893b806
glitzerbargeld <torben.jaeckel@me.com>
2021-06-26 12:49:34 +0200

    xx

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -32,7 +32,7 @@ if ( post_password_required() ) {
 }
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
-<div class="ast-row">
+<div class="ast-row product-row">
 
 	<div class="ast-col-md-5 ast-col-xs-12 ast-col-md-push-7">
 		<?php
